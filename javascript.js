23e3b9d1737795664a1c9d4be8f633242370081f
ChrisPapaventsis <chrisalexpapav2006@gmail.com>
2024-06-13 12:20:19 +0300

    playRound function done

--- javascript.js
+++ javascript.js
@@ -23,3 +23,25 @@ function getHumanChoice() {
 }
 
 let humanScore = 0, computerScore = 0;
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice == "rock" && humanChoice == "paper") {
+        console.log("You win! Paper beats Rock.");
+        humanScore ++;
+    } else if (computerChoice == "rock" && humanChoice == "scissors") {
+        console.log("You lose! Rock beats Scissors.");
+        computerScore ++;
+    } else if (computerChoice == "paper" && humanChoice == "rock") {
+        console.log("You lose! Paper beats Rock.");
+        computerScore ++;
+    } else if (computerChoice == "paper" && humanChoice == "scissors") {
+        console.log("You win! Scissors beats Paper.");
+        humanScore ++;
+    } else if (computerChoice == "scissors" && humanChoice == "rock") {
+        console.log("You win! Rock  beats Scissors.");
+        humanScore ++;
+    } else if (computerChoice == "scissors" && humanChoice == "paper") {
+        console.log("You lose! Scissors beats Paper.");
+        computerScore ++;
+    }
+}
